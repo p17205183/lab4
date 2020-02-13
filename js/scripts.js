a7bb6bef1a12d8ac9cbbf35fe3b785ae639fc57b
Rosie Sivertsen <P17205183@LEC-ADMIN.dmu.ac.uk>
2020-02-13 10:29:00 +0000

    lab4 completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+toggleMenu = () => {
+  console.log("Called toggleMenu");
+  menuNav.classList.toggle("menu-toggle");
+}
+
+menuToggle.addEventListener('click', toggleMenu);
